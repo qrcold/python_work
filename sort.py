24208acc34260248c59616f9f91f06e46a1b56d8
qrcold <qrcolds@outlook.com>
2026-01-24 15:55:12 +0800

    尝试使用临时排序而非永久排序 Implement temporary sorting instead of permanent sorting

--- sort.py
+++ sort.py
@@ -3,11 +3,14 @@
 # and remenber sets are unordered collections of unique elements
 # here is an example of sorting a set
 name=['apple','candy','banana','pie']
-name.sort()
+# name.sort()
+# print(name)
+# name.sort(reverse=True)
+# print(name)
+print("Here is the sorted list:\n")
+print(sorted(name))
+print("Here is the original list:")
 print(name)
-name.sort(reverse=True)
-print(name)
-
 # to run this code, save it in a file named 'sort.py'
 # then in 'terminal' input 'python sort.py' to run the code
 # and then you will see the sorted list printed out
