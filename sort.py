e4144dc51d91d1b2776aa31450a7d636152d4caa
qrcold <qrcolds@outlook.com>
2026-01-24 17:05:14 +0800

    测试len()函数的使用情况 to test len() function usage

--- sort.py
+++ sort.py
@@ -9,21 +9,10 @@ name=['apple','candy','banana','pie']
 # print(name)
 print("Here is the sorted list:\n")
 print(sorted(name))
+len_name=len(name)
+print(f"The length of the list is {len_name}")
 # print("Here is the original list:")
 # print(name)
-temp=name
-name.reverse()
-print("Here is another reversed list:")
-print(name)
-temp.sort(reverse=True)
-print("Here is another sorted list in descending order:")
-print(temp)
-print("actually there's difference between .reverse() and reverse=True")
-print("because .reverse() changes the order of the original list")
-print("while reverse=True just displays the list in descending order")
-print("so the original list is now changed to:")
-print(name)
-print("notice that temp is just a reference to name")
 # to run this code, save it in a file named 'sort.py'
 # then in 'terminal' input 'python sort.py' to run the code
 # and then you will see the sorted list printed out
