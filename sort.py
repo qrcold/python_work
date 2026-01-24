a523474ad6b0bf2ec7dd3796811d764579f20edf
qrcold <qrcolds@outlook.com>
2026-01-24 16:54:13 +0800

    比较reverse.()和sort(reverse=True)的差异 即前者是对列表进行原地反转（不排序，只改变位置），而后者是对列表进行排序然后升序排列 compare reverse.() and sort(reverse=True) That is, the former reverses the list in place (not sorting, just changing positions), while the latter sorts the list in descending order.

--- sort.py
+++ sort.py
@@ -11,9 +11,19 @@ print("Here is the sorted list:\n")
 print(sorted(name))
 # print("Here is the original list:")
 # print(name)
+temp=name
 name.reverse()
 print("Here is another reversed list:")
 print(name)
+temp.sort(reverse=True)
+print("Here is another sorted list in descending order:")
+print(temp)
+print("actually there's difference between .reverse() and reverse=True")
+print("because .reverse() changes the order of the original list")
+print("while reverse=True just displays the list in descending order")
+print("so the original list is now changed to:")
+print(name)
+print("notice that temp is just a reference to name")
 # to run this code, save it in a file named 'sort.py'
 # then in 'terminal' input 'python sort.py' to run the code
 # and then you will see the sorted list printed out
