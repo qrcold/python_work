84da7a4f82614c27d0850a849aab1d4bcdedb596
qrcold <qrcolds@outlook.com>
2026-03-20 16:17:30 +0800

    复习缩进 review indentation

--- for_loop.py
+++ for_loop.py
@@ -2,3 +2,5 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians: 
     print(magician) #note the indentation, it is important in py to indent the code that belongs to the loop
     print(f"{magician.title()}, that was a great trick!") #you can use f-strings to format the output, and you can use string methods like title() to modify the string before printing it
+    print(f"I can't wait to see your next trick, {magician.title()}.\n") #you can also add a newline character \n to create a blank line after each magician's message
+print("Thank you, everyone. That was a great magic show!") #this line is not indented, so it will be executed after the loop has finished, and it will only be printed once
